69a39c625cac6c0f9f5b3a61f3db67999b090f36
rdesitter <rdesitter@gmail.com>
2022-12-09 09:56:35 +0100

    fix header menu toggle only on mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,20 +6,26 @@ import './style.scss';
 const Header = ({ isDark }) => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
+    const toggleMenu = (active) => {
+        if (active) { 
+            setIsOpen(!isOpen);
+        } else {
+            if(isOpen) {
+                setIsOpen(false)
+            }
+        }
     }
     
     return (
         <header className={isDark ? 'header' : 'header light'}>
             <div className="header__container">
                 <div className="header__logo"><h1><Link className="header__logo--link" to="/" as="h1">Raphael Desitter</Link></h1></div>
-                <button className={isOpen ? 'header__button open': 'header__button'} id="btn" onClick={toggleMenu}><i className="header__button--icon"></i><span>menu</span></button>
+                <button className={isOpen ? 'header__button open': 'header__button'} id="btn" onClick={() => toggleMenu(true)}><i className="header__button--icon"></i><span>menu</span></button>
                 <nav className={isOpen ? 'header__menu visible': 'header__menu'}>
                     <ul className="header__menu__list">
-                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/" onClick={toggleMenu}>Accueil</NavLink></li>
-                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/portfolio" onClick={toggleMenu}>Portfolio</NavLink></li>
-                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/a-propos" onClick={toggleMenu}>A propos</NavLink></li>
+                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/" onClick={() => toggleMenu(false)}>Accueil</NavLink></li>
+                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/portfolio" onClick={() => toggleMenu(false)}>Portfolio</NavLink></li>
+                        <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/a-propos" onClick={() => toggleMenu(false)}>A propos</NavLink></li>
                         {/* <li className="header__menu__list--item"><NavLink className="header__menu__list--link" to="/contact" onClick={toggleMenu}>Me contacter</NavLink></li>  */}
                     </ul>
                 </nav>
